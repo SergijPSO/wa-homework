6f86eba20410b7965e5ece154a967db6f17b5c2e
Pankiv S <sergiy.p@deepinspire.com>
2021-10-17 18:36:23 +0300

    Final solution was added

--- Assignment_9/Assignment_9/ViewController.swift
+++ Assignment_9/Assignment_9/ViewController.swift
@@ -12,20 +12,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
 
     //Creating dictionary with group members
-    var usersDictionary = ["Slipi":"Sergiy Pankiv", "bazinga":"Artem Velykyy", "Artem1":"Artem Zosyuk", "Artem2":"Artem Bilous", "p.starychok": "Petro", "Valentina":"Valentina", "Eugene":"Eugene Vlasov", "Dima": "Dima Paliyenko", "Valerij":"Valerij Syroiezhin", "Anton":"Anton", "Mykita":"Mykita Babushkin"]
+    var usersDictionary = ["Slipi":"Sergiy Pankiv", "bazinga":"Artem Velykyy", "Artem1":"Artem Zosyuk", "Artem2":"Artem Bilous", "p.starychok": "Petro", "Valentina":"Valentina", "Eugene":"Eugene Vlasov", "Dima": "Dima Paliyenko", "Valerij":"Valerij Syroiezhin", "Anton":"Anton", "Mykita":"Mykita Babushkin", "Viktor": "Viktor Mandryk"]
 
-    var usersNames = [String]()
     var usersNicks = [String]()
+    var nicksArr = [String]()
+    var fullNamesArr = [String]()
 
-// MARK: Lyfececle
+    // MARK: Lyfecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
-        usersNames = Array(usersDictionary.values)
         usersNicks = Array(usersDictionary.keys)
     }
 }
-
 
 extension ViewController: UITableViewDataSource {
 
@@ -35,30 +34,30 @@ extension ViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "IndexPathCell", for: indexPath)
-
-        for (index, element) in usersDictionary.enumerated() {
-
-            element.key.capitalized
-            element.value.capitalized
-        }
-        print(usersDictionary)
-
+        // Casting keys to lowercase
         for (index, element) in usersNicks.enumerated() {
-            usersNicks[index] = element.capitalized
+            usersNicks[index] = element.lowercased()
         }
-
-        for (index, element) in usersNames.enumerated() {
-            usersNames[index] = element.capitalized
-        }
-
-        let sortedNames = usersNames.sorted()
+        // Sorting keys in alhabet order
         let sortedNicks = usersNicks.sorted()
 
-        cell.textLabel?.text = sortedNicks[indexPath.row]
-        cell.detailTextLabel?.text = sortedNames[indexPath.row]
+        for element in sortedNicks {
+            //Cycle for comparing dictionary keys and sorted array
+            for item in usersDictionary {
+                let userNick = item.key
+                let userFullName = item.value
+                //Creating new separate already sorted arrays with users nicks and users fullNmaes
+                if userNick.lowercased() == element.lowercased() {
+                    nicksArr.append(userNick)
+                    fullNamesArr.append(userFullName)
+                }
+            }
+        }
+
+        //Setting title and subtitle text
+        cell.textLabel?.text = nicksArr[indexPath.row]
+        cell.detailTextLabel?.text = fullNamesArr[indexPath.row]
 
         return cell
     }
-
-
 }
